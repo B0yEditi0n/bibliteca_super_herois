fa05e9193584d189aa453e5f1c02beb230bfa664
B0yEditi0n <ccaioabreudesouza9874@gmail.com>
2024-06-08 16:22:38 -0300

    primeiro comit

---
+++ lib/bibliteca_super_herois.dart
@@ -0,0 +1,25 @@
+
+class Ficha{
+  String nomePersonagem = '';
+
+  // Habilidades
+  List<Object> Habilidades = [
+    {"FOR": 0},
+    {"VIG": 0},
+    {"AGI": 0},
+    {"DES": 0},
+    {"LUT": 0},
+    {"INT": 0},
+    {"PRO": 0},
+    {"PRE": 0}
+  ];
+
+  List<Object> Defesas =[
+    {"Esquiva": 0},
+    {"Aparar": 0},
+    {"Fortitude": 0},
+    {"Vontade": 0},
+  ];
+
+  List<String> complicacoes = [];
+}
---
+++ test/bibliteca_super_herois_test.dart
@@ -0,0 +1,6 @@
+import 'package:bibliteca_super_herois/bibliteca_super_herois.dart';
+import 'package:test/test.dart';
+
+void main() {
+
+}
