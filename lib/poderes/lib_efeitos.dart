97140a6fc91cb08baa2d702521f259017802e381
B0yEditi0n <ccaioabreudesouza9874@gmail.com>
2024-06-09 11:22:40 -0300

    Configuração de Efito, implmentado inicialização e retorno de reimplementação

--- lib/poderes/lib_efeitos.dart
+++ lib/poderes/lib_efeitos.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+import 'dart:io';
+import 'dart:convert';
+
 class Efeito{
   String nome = '' ;
   String _nomeEfeito = '';
@@ -5,23 +9,73 @@ class Efeito{
   int _custoBase = 0;
   int graduacao = 0;
 
+  int acao = -1;
+  int alcance = -1;
+  int duracao = -1;
+  
+  Future<bool> instanciarMetodo(String nome, String idEfeito) async{
+    /*
+      Carrega os atributos básicos do efeito 
+      o algoritimo que o chamar precisa usar await
+      para carregar o json
 
+      Args:
+        nome     - nome do poder a ser dados
+        idEfeito - id do efeito a ser inserido
+      Return:
+        Map Json - o Arquivo json
+    */
+    
+    nome = nome;
+    _idEfeito = idEfeito;
 
-  bool instanciarMetodo(objEfeito){
-    // Preenchimento dos Atributos
-    _idEfeito = objEfeito["e_id"];
-    _nomeEfeito = objEfeito["efeito"];
-    _custoBase = objEfeito["custo_base"];
+    // carrega json Base    
+    var efeitos = await carregaJson('efeitos');
+    var efeitoAtual = efeitos["EFECTS"][efeitos["EFECTS"].indexWhere((efeito) => efeito["e_id"] == idEfeito)];
+    
+    //Preenche atributos do objeto
+    _custoBase = efeitoAtual["custo_base"];
+    acao       = efeitoAtual["acao"];
+    alcance    = efeitoAtual["alcance"];
+    duracao    = efeitoAtual["duracao"];
 
     return true;
   }
 
+
+  Future carregaJson(String json) async{
+    /*
+      carrega qualquer arquivo json necessário dentro do projeto
+
+      Args:
+        String json - nome do arquivo sem extensão
+      Return:
+        Map Json - o Arquivo json
+    */
+
+    String currentDirectory = Directory.current.path;
+    var jsonEfeitos = await File('${currentDirectory}/lib/poderes/${json}.json').readAsString();
+    var objetoJson = jsonDecode(jsonEfeitos);
+
+    return(objetoJson);
+
+  }
+
   Map<String, dynamic> retornaObj(){
+    /*
+      Retorna um json com os dados montados
+
+      Return:
+        Map Json - o Arquivo json
+    */
+
     return{
-      "e_id": _idEfeito,
-      "efeito": _nomeEfeito,
-      "custo_base": _custoBase,
+      "e_id":      _idEfeito,
+      "efeito":    _nomeEfeito,
       "graduacao": graduacao,
+      "acao":      acao,
+      "alcance":   alcance,
+      "duracao":   duracao,
       
     };
   }
--- test/bibliteca_super_herois_test.dart
+++ test/bibliteca_super_herois_test.dart
@@ -1,6 +1,9 @@
 import 'package:bibliteca_super_herois/bibliteca_super_herois.dart';
+import 'package:bibliteca_super_herois/poderes/lib_efeitos.dart';
 import 'package:test/test.dart';
 
-void main() {
 
+void main() async{
+  var poder = Efeito();
+  await poder.instanciarMetodo('Poder01', 'E004');
 }
